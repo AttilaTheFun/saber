7084302515377d46d1575626838d461d0a61e199
Logan Shire <3255391+AttilaTheFun@users.noreply.github.com>
2024-07-06 17:03:29 -0400

    Update RSPM and delete swift deps index

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Packages",
     dependencies: [
-
-        // MARK: First Party Dependencies
-
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
         .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.2"),
     ]
 )
